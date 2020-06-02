6bb248fc261ef8216e5ad659a1f6f6899139a7b1
Gustavo Martins dos Santos <gustavo_mts2005@hotmail.com>
2020-06-02 19:06:52 -0300

    Doing some tests on the work tool

--- Exercicio1/Program.cs
+++ Exercicio1/Program.cs
@@ -11,7 +11,7 @@ namespace Exercicio1 {
             int num = int.Parse(Console.ReadLine());
             int num2 = int.Parse(Console.ReadLine());
             // Hello Github
-
+            // Hello Visual Studio and Github
             if (num != num2) {
                 if (num > num2)
                     Console.WriteLine("O maior desses números é {0}", num);
