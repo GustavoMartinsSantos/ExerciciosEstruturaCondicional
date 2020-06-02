739b34c0e2cab020283c969f1857678d70f680ee
Gustavo Martins dos Santos <62625567+GustavoMartinsSantos@users.noreply.github.com>
2020-06-01 22:09:33 -0300

    Adding Exercise1

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Escreva dois valores: ");
+            int num = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            if (num != num2) {
+                if (num > num2)
+                    Console.WriteLine("O maior deles é {0}", num);
+                else
+                    Console.WriteLine("O maior deles é {0}", num2);
+            }
+        }
+    }
+}
