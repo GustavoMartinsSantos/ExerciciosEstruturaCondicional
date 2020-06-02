4e228c0f0040eb27dc618123c1408a6c4d5f15fd
Gustavo Martins dos Santos <62625567+GustavoMartinsSantos@users.noreply.github.com>
2020-06-01 22:28:09 -0300

    Adding Exercise2

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite o seu ano de nascimento: ");
+            int ano = int.Parse(Console.ReadLine());
+
+            if (ano >= 0) {
+                ano = 2020 - ano;
+
+                if (ano >= 18)
+                    Console.WriteLine("Você deve votar este ano!");
+                else 
+                    Console.WriteLine("Você ainda não pode votar.");
+            }
+        }
+    }
+}
