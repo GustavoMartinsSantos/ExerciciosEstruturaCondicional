b2e01e96d6b7d808bb94877ea4c586adb2f53383
Gustavo Martins dos Santos <62625567+GustavoMartinsSantos@users.noreply.github.com>
2020-06-01 22:32:23 -0300

    Adding Exercises 3 and 4

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3 {
+    class Program {
+        static void Main(string[] args) {
+            string senha = "1234";
+
+            Console.Write("Digite a senha: ");
+            string valor = Console.ReadLine();
+
+            if (valor == senha)
+                Console.WriteLine("ACESSO PERMITIDO");
+            else
+                Console.WriteLine("ACESSO NEGADO");
+        }
+    }
+}
---
+++ Exercicio4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Digite o número de maçãs que você vai comprar: ");
+            int numacas = int.Parse(Console.ReadLine());
+            double valor;
+
+            if (numacas < 12 && numacas > 0)
+                valor = numacas * 0.30;
+            else if (numacas >= 12)
+                valor = numacas * 0.25;
+            else
+                valor = 0;
+
+            Console.WriteLine("O valor total é igual a {0}", valor + "R$");
+        }
+    }
+}
